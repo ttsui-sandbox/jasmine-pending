34d1ee888e6b0647c6dabe7b815cfc4b731edf72
Tony Tsui <tsui.tony@gmail.com>
2015-01-12 09:51:24 +0000

    Remove debugging code and fix indentation.

--- src/jasmine-pending.js
+++ src/jasmine-pending.js
@@ -1,17 +1,16 @@
-  function pending_it(description, fn) {
-    var spec = it('PENDING: ' + description, fn);
+function pending_it(description, fn) {
+  var spec = it('PENDING: ' + description, fn);
 
-    spec.addExpectationResult = function(passed, result) {
-      console.log('Called addExpectationResult');
+  spec.addExpectationResult = function(passed, result) {
+    if (passed) {
+      result.message = 'Unexpected success';
+      result.passed = false;
       
-      if (passed) {
-        result.message = 'Unexpected success';
-        result.passed = false;
-        return spec.__proto__.addExpectationResult.call(this, false, result);
-      }
+      return spec.__proto__.addExpectationResult.call(this, false, result);
+    }
 
-      return spec.__proto__.addExpectationResult.call(this, true, result);
-    };
+    return spec.__proto__.addExpectationResult.call(this, true, result);
+  };
 
-    return spec;
-  }
+  return spec;
+}
